50b247c5fe3c3c02771cd5a260f1406c4c818cd4
Alexander Lagerström <edika32@gmail.com>
2016-09-21 06:38:38 +0200

    Changes the startup script for subsonic
    Whenever the transcode file didn't exist in the /subsonic
    directory the startup failed.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -13,6 +13,10 @@ source /etc/default/subsonic
 # Both $SUBSONIC_HOME and #SUBSONIC_ARGS are sourced from
 # the config file.
 
+if [ ! -d $SUBSONIC_HOME/transcode/ ]; then
+    mkdir $SUBSONIC_HOME/transcode/
+fi
+
 if [ ! -f "$SUBSONIC_HOME/transcode/ffmpeg" ]; then
     cp /var/subsonic/transcode/ffmpeg "$SUBSONIC_HOME/transcode/ffmpeg"
 fi
@@ -20,6 +24,10 @@ fi
 if [ ! -f "$SUBSONIC_HOME/transcode/lame" ]; then
     cp /var/subsonic/transcode/lame "$SUBSONIC_HOME/transcode/lame"
 fi
+
+# Changes the owner of all files in /subsonic to
+# the subsonic user
+chown $SUBSONIC_USER:$SUBSONIC_USER -R $SUBSONIC_HOME
 
 su $SUBSONIC_USER -c "/usr/bin/subsonic $SUBSONIC_ARGS"
 sleep 5
